b292b92ed4345ad89351ebd7b0430f4ab7578eb2
Michael Taggart <mtaggart@taggart-tech.com>
2023-05-30 18:24:21 -0700

    Start lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod injectors;
+use injectors::{
+    InjectorType,
+    Injector
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn init(injector_type: InjectorType ) -> Box<dyn Injector> {
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
